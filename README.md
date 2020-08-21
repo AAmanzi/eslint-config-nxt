0e3701818953851080b3de90f254f5c9bad72c90
alex.amanzi <alex.amanzi@seekandhit.com>
2020-08-21 12:20:04 +0200

    add eslint config

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-nxt
+
+A shared [ESLint](https://eslint.org/) config based on [airbnb-base](https://www.npmjs.com/package/eslint-config-airbnb-base)
---
+++ index.js
@@ -0,0 +1,70 @@
+module.exports = {
+  extends: [
+    "airbnb-base",
+    "plugin:jest/all",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["jest", "@typescript-eslint"],
+  root: true,
+  globals: {},
+  rules: {
+    "import/no-unresolved": [2, { commonjs: true, amd: true }],
+    "import/prefer-default-export": "off",
+    "max-len": [
+      "error",
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/extensions": "off",
+    "no-plusplus": "off",
+    "implicit-arrow-linebreak": "off",
+    "operator-linebreak": "off",
+    "no-confusing-arrow": "off",
+    "arrow-body-style": "off",
+    "function-paren-newline": "off",
+    "no-restricted-syntax": "off",
+    "object-curly-newline": "off",
+    "no-await-in-loop": "off",
+  },
+  parser: "@typescript-eslint/parser",
+  env: {},
+  overrides: [],
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [
+          ["@src", "./src"],
+          ["@tests", "./tests"],
+          ["@queries", "./queries"],
+        ],
+        extensions: [".ts", ".js"],
+      },
+    },
+  },
+};
